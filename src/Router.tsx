0bb05ec7d9317773e6a0a59d006d2ab53cf0c305
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-09-22 19:52:07 +0900

    🔧 chore: path 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import App from "@/App";
+import { Detail, Home } from "@pages/index";
+import { ROUTE_PATH } from "@constants/index";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: ROUTE_PATH.ROOT,
+    element: <App />,
+    children: [
+      {
+        path: ROUTE_PATH.HOME,
+        element: <Home />,
+      },
+      {
+        path: ROUTE_PATH.DETAIL,
+        element: <Detail />,
+      },
+    ],
+  },
+]);
 
 export default router;
---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const API_BASE_URL = "https://disney_api.nomadcoders.workers.dev";
+
+export const ROUTE_PATH = {
+  ROOT: "/",
+  HOME: "",
+  DETAIL: "character/:id",
+} as const;
