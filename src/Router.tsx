5dcdff487e40f9a16f9a0be677858ad5ad8e83dc
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-09-23 13:43:40 +0900

    ✨ feat: 홈 로더 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+
 import App from "@/App";
-import { Detail, Home } from "@/pages";
+import { Detail, Home, homeLoader } from "@/pages";
 import { ROUTE_PATH } from "@/constants";
+
+const queryClient = useQueryClient();
 
 const router = createBrowserRouter([
   {
@@ -11,6 +15,7 @@ const router = createBrowserRouter([
       {
         path: ROUTE_PATH.HOME,
         element: <Home />,
+        loader: homeLoader(queryClient),
       },
       {
         path: ROUTE_PATH.DETAIL,
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,2 @@
-export { default as Home } from "./home";
+export { default as Home, loader as homeLoader } from "./home";
 export { default as Detail } from "./detail";
