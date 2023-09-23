0c37c35a6c3ddf1fe0b995d61d6ea8452554c92b
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-09-23 13:43:49 +0900

    feat: 홈 페이지 구현

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,35 @@
+import { QueryClient } from "@tanstack/react-query";
+import styled from "styled-components";
+
+import { CharacterItem } from "@/components";
+import { useCharactersQuery, charactersQuery } from "@/hooks";
+import { characterService } from "@/services";
+
 const Home = () => {
-  return <></>;
+  const { characters, isLoading } = useCharactersQuery(characterService);
+
+  return (
+    <Container>
+      {characters?.slice(0, 100).map((character) => (
+        <CharacterItem key={character.id} character={character} />
+      ))}
+    </Container>
+  );
 };
+
+export const loader = (queryClient: QueryClient) => async () => {
+  const query = charactersQuery({ service: characterService });
+  return (
+    queryClient.getQueryData(query.queryKey) ??
+    (await queryClient.fetchQuery(query))
+  );
+};
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 20px;
+  margin-top: 40px;
+`;
 
 export default Home;
